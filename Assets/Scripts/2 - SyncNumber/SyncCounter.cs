fb52ab661df2b21b985547ac6298f733cadb60ed
hatuxes <hatuxes@gmail.com>
2020-04-08 14:38:19 +0900

    [Update] Changed master judgment method.

--- Assets/Scripts/2 - SyncNumber/SyncCounter.cs
+++ Assets/Scripts/2 - SyncNumber/SyncCounter.cs
@@ -32,7 +32,7 @@ public class SyncCounter : UdonSharpBehaviour
     public override void Interact()
     {
         //Cubeに対するオーナーを変更
-        if (Networking.GetOwner(this.gameObject) != Networking.LocalPlayer) Networking.SetOwner(Networking.LocalPlayer, this.gameObject);
+        if (!Networking.IsOwner(Networking.LocalPlayer, this.gameObject)) Networking.SetOwner(Networking.LocalPlayer, this.gameObject);
         //処理同期
         SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "CountUp");
     }
